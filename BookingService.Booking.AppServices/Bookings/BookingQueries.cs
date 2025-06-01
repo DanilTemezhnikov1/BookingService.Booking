9dfa0a8b0f5dcaa698c4521046db0599d23bfae0
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-01 19:36:17 +0300

    реализация интерфейса IBookingsQueries

---
+++ BookingService.Booking.AppServices/Bookings/BookingQueries.cs
@@ -0,0 +1,22 @@
+﻿using BookingService.Booking.Api.Contracts.Bookings.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingService.Booking.AppServices.Bookings
+{
+    internal class BookingQueries : IBookingsQueries
+    {
+        public BookingData[] GetByFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookingData.BookingStatus GetStatusById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
