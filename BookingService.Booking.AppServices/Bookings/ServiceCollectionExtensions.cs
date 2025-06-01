a1becca16d7f968cee7bf2d4cd2b21fef6374a82
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-01 19:42:09 +0300

    создал статический метод расширения для IServiceCollection

---
+++ BookingService.Booking.AppServices/Bookings/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingService.Booking.AppServices.Bookings
+{
+    internal static class ServiceCollectionExtensions
+    {
+        static void AddAppServices(this IServiceCollection services) { }
+    }
+}
