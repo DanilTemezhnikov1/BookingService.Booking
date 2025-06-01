3483ea8ace0f1a19b92901ed00cba81e46a109df
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-01 18:35:41 +0300

    добавление Bookings

---
+++ BookingService.Booking.Api.Contracts/Bookings/Dtos/BookingData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingService.Booking.Api.Contracts.Bookings.Dtos
+{
+    internal class BookingData
+    {
+        public enum BookingStatus
+        {
+            AwaitsConfirmation,
+            Confirmed,
+            Cancelled
+        }
+
+        public long Id;
+        public BookingStatus Status;
+        public long IdUser;
+        public long IdBooking;
+        public DateOnly StartBooking;
+        public DateOnly EndBooking;
+        public DateTimeOffset CreationBooking;
+    }
+}
