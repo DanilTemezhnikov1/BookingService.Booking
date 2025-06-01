38f00016e67f727e41476707e47edd69514bffb7
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-01 19:29:56 +0300

    реализация интерфейса IBookingsService

---
+++ BookingService.Booking.AppServices/Bookings/BookingService.cs
@@ -0,0 +1,26 @@
+﻿using BookingService.Booking.Api.Contracts.Bookings.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingService.Booking.AppServices.Bookings
+{
+    internal class BookingService : IBookingsService
+    {
+        public long Create(long Id, BookingData.BookingStatus Status, long IdUser, long IdBooking, DateOnly StartBooking, DateOnly EndBooking, DateTimeOffset CreationBooking)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BookingData GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Cancel(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
