791030269a1703abffc63417e0cfca26225a59a1
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-03 21:29:36 +0300

    вызвал стартап

--- BookingService.Booking.Host/Program.cs
+++ BookingService.Booking.Host/Program.cs
@@ -1,5 +1,9 @@
 ﻿
-    var builder = WebApplication.CreateBuilder(args);
-    var app = builder.Build();
-    app.MapGet("/", () => "Hello World!");
-    app.Run();
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+//app.MapGet("/", () => "Hello World!");
+//app.Run();
+using BookingService.Booking.Host;
+
+var host = HostBuilderFactory.BuildHost(args);
+host.Run();
