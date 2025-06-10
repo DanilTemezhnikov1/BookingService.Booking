d1d57876206aefe5adaf049298f1fb041fde29a1
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-10 19:27:17 +0300

    добавил регистрацию с помощью метода ApplyConfiguration

---
+++ src/BookingService.Booking.Persistence/BookingsContext.cs
@@ -0,0 +1,22 @@
+﻿using BookingService.Booking.Domain.Bookings;
+using BookingService.Booking.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingService.Booking.Persistence
+{
+    public class BookingsContext : DbContext
+    {
+        public DbSet<BookingAggregate> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+            modelBuilder.ApplyConfiguration<BookingAggregate>(new BookingAggregateConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
