5f15807e2310c0ce788e07301ac4b55a0b7cdd14
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-12 18:49:15 +0300

    Добавил подключение RestEase клиента для сервиса Catalog в метод AddAppservices

--- src/BookingService.Booking.AppServices/Bookings/ServiceCollectionExtensions.cs
+++ src/BookingService.Booking.AppServices/Bookings/ServiceCollectionExtensions.cs
@@ -1,6 +1,11 @@
 ﻿using BookingService.Booking.AppServices.Dates;
+using BookingService.Booking.AppServices.Options;
+using BookingService.Catalog.Api.Contracts.BookingJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RestEase;
+using System.Net.Http;
 
 namespace BookingService.Booking.AppServices.Bookings
 {
@@ -10,6 +15,18 @@ namespace BookingService.Booking.AppServices.Bookings
         {
             services.AddScoped<IBookingsService, BookingService>();
             services.AddSingleton<ICurrentDateTimeProvider, DefaultCurrentDateTimeProvider>();
+
+            services.Configure<BookingCatalogRestOptions>(configuration.GetSection(nameof(BookingCatalogRestOptions))); 
+            services.AddHttpClient(nameof(BookingCatalogRestOptions),
+   (ctx, client) =>
+   {
+       var options = ctx.GetRequiredService<IOptions<BookingCatalogRestOptions>>().Value;
+       client.BaseAddress = new Uri(options.BaseAddress);
+       client.Timeout = TimeSpan.FromSeconds(90);
+   });
+
+            services.AddScoped<IBookingJobsController>(ctx => RestClient.For<IBookingJobsController>(ctx.GetRequiredService<IHttpClientFactory>()
+      .CreateClient(nameof(BookingCatalogRestOptions))));
         }
     }
 }
