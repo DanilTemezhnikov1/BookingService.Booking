82516a8cf1b71f9596699dc7ddf43396fa69e7ed
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-12 19:06:14 +0300

    Настроил политику повторных вызовов на основе библиотеки Polly для зарегистрированного http-клиента

--- src/BookingService.Booking.AppServices/Bookings/ServiceCollectionExtensions.cs
+++ src/BookingService.Booking.AppServices/Bookings/ServiceCollectionExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using RestEase;
-using System.Net.Http;
+using Polly;
 
 namespace BookingService.Booking.AppServices.Bookings
 {
@@ -17,13 +17,16 @@ namespace BookingService.Booking.AppServices.Bookings
             services.AddSingleton<ICurrentDateTimeProvider, DefaultCurrentDateTimeProvider>();
 
             services.Configure<BookingCatalogRestOptions>(configuration.GetSection(nameof(BookingCatalogRestOptions))); 
+
             services.AddHttpClient(nameof(BookingCatalogRestOptions),
    (ctx, client) =>
    {
        var options = ctx.GetRequiredService<IOptions<BookingCatalogRestOptions>>().Value;
        client.BaseAddress = new Uri(options.BaseAddress);
        client.Timeout = TimeSpan.FromSeconds(90);
-   });
+   }).AddTransientHttpErrorPolicy(builder => builder
+   .WaitAndRetryAsync(4, retryAttempt => 
+   TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))));
 
             services.AddScoped<IBookingJobsController>(ctx => RestClient.For<IBookingJobsController>(ctx.GetRequiredService<IHttpClientFactory>()
       .CreateClient(nameof(BookingCatalogRestOptions))));
