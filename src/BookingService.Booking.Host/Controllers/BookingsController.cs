c4ef5bbd7f902a4cfe621734de768794c64699be
Даниил Темежников <radio_gnezdo-06@mail.ru>
2025-06-19 21:05:26 +0300

    изменил источник CancellationToken

--- src/BookingService.Booking.Host/Controllers/BookingsController.cs
+++ src/BookingService.Booking.Host/Controllers/BookingsController.cs
@@ -12,47 +12,45 @@ public class BookingsController : ControllerBase
 {
     private readonly IBookingsQueries _bookingsQueries;
     private readonly IBookingsService _bookingsService;
-    private CancellationToken _cancellationToken;
 
     public BookingsController(IBookingsService bookingsService, IBookingsQueries bookingsQueries, CancellationToken cancellationToken)
     {
         _bookingsService = bookingsService;
         _bookingsQueries = bookingsQueries;
-        _cancellationToken = cancellationToken;
     }
 
     [HttpPost]
     [Route(WebRoutes.Create)]
-    public async Task<long> CreateBooking([FromBody] CreateBookingRequest createBookingRequest)
+    public async Task<long> CreateBooking([FromBody] CreateBookingRequest createBookingRequest, CancellationToken cancellationToken)
     {
-        return await _bookingsService.Create(createBookingRequest.ToQuery(), _cancellationToken);
+        return await _bookingsService.Create(createBookingRequest.ToQuery(), cancellationToken);
     }
 
     [HttpGet]
     [Route(WebRoutes.GetByFilter)]
-    public async Task<BookingData[]> GetBookingsByFilter([FromQuery] GetBookingsByFilterRequest getBookingsByFilter)
+    public async Task<BookingData[]> GetBookingsByFilter([FromQuery] GetBookingsByFilterRequest getBookingsByFilter, CancellationToken cancellationToken)
     {
-        return await _bookingsQueries.GetByFilter(getBookingsByFilter.ToQuery(), _cancellationToken);
+        return await _bookingsQueries.GetByFilter(getBookingsByFilter.ToQuery(), cancellationToken);
     }
 
     [HttpPost]
     [Route(WebRoutes.Cancel)]
-    public async Task CancelBooking([FromBody] long id)
+    public async Task CancelBooking([FromBody] long id, CancellationToken cancellationToken)
     {
-        await _bookingsService.Cancel(id, _cancellationToken);
+        await _bookingsService.Cancel(id, cancellationToken);
     }
 
     [HttpGet]
     [Route(WebRoutes.GetById)]
-    public async Task<BookingData> GetBookingById([FromRoute] long id)
+    public async Task<BookingData> GetBookingById([FromRoute] long id, CancellationToken cancellationToken)
     {
-        return await _bookingsService.GetById(id, _cancellationToken);
+        return await _bookingsService.GetById(id, cancellationToken);
     }
 
     [HttpGet]
     [Route(WebRoutes.GetStatusById)]
-    public async Task<string> GetBookingStatusById([FromRoute] long id)
+    public async Task<string> GetBookingStatusById([FromRoute] long id, CancellationToken cancellationToken)
     {
-        return await _bookingsQueries.GetStatusById(id, _cancellationToken);
+        return await _bookingsQueries.GetStatusById(id, cancellationToken);
     }
 }
